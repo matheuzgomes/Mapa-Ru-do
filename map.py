1989595110dafa7007ee112743f6bc0c5ac9b1aa
aluno07 <aluno07@TSACBRRJLP045.acad.tsag>
2022-05-18 14:32:54 -0300

    adding input

--- map.py
+++ map.py
@@ -1,27 +1,50 @@
 import folium
 import requests
 import urllib.parse
+import pandas as pd
 
-address = input("Endereço: ")
-url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(address) +'?format=json'
 
-response = requests.get(url).json()
-lat = response[0]['lat']
-lon = response[0]['lon']
+df = pd.DataFrame(columns=['Rua', 'Bairro', 'Estado'])
 
-my_map1 = folium.Map(location= [lat, lon], zoom_start=12)
+for _ in range(2):
+    ra = input('Qual a rua: ')
+    ba = input('Agora o Bairro: ')
+    es = input('E o estado: ')
+    df1 = pd.DataFrame(data=[[ra,ba,es]], columns=['Rua', 'Bairro', 'Estado'])
+    df = pd.concat([df,df1], axis=0)
 
-value = input("Qual o tipo de barulho se encontra nesta área: ")
+my_map = folium.Map()
 
-folium.Marker([lat, lon], popup=value).add_to(my_map1)
+for label, row in df.iterrows():
+    rua = row['Rua']
+    bairro = row['Bairro']
+    teste = [rua,bairro]
+    print(teste)
+    teste_str = str(teste)
+    print(teste_str)
+    # address = input("Endereço: ")
+    url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(teste_str) +'?format=json'
 
-folium.CircleMarker(
-    radius = 50,
-    location=[lat, lon],
-    popup= 'Barulho pra caralho',
-    color = "#3186cc",
-    fill = True,
-    fill_color = 'red'
-).add_to(my_map1)
+    response = requests.get(url).json()
+    lat = response[0]['lat']
+    lon = response[0]['lon']
 
-my_map1.save('my_map1.html')
+    folium.Marker([lat, lon]).add_to(my_map)
+
+my_map.location = [lat, lon]
+my_map.options['zoom'] = 12
+
+    # folium.CircleMarker(
+    #     radius = 50,
+    #     location=[lat, lon],
+    #     popup= 'Barulho pra caralho',
+    #     color = "#3186cc",
+    #     fill = True,
+    #     fill_color = 'red'
+    # ).add_to(my_map1)
+
+# value = input("Qual o tipo de barulho se encontra nesta área: ")
+
+
+
+my_map.save('my_map1.html')
